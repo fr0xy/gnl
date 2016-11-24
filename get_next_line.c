64e67f6f4e15309f56554c769c5e7229afc0d487
Remy CHOFFARDET <rchoffar@e1r5p10.42.fr>
2016-11-24 18:16:56 +0100

    modif v2

--- get_next_line.c
+++ get_next_line.c
@@ -22,46 +22,71 @@ int	ft_test_tab_line(char *str, int *i)
 
 int	get_next_line(const int fd, char **line)
 {
-	static char	*tab_line[214748364];
+	static char	*tab_line[255];
 	char	buf[BUFF_SIZE + 1];
 	char	*tmp;
 	int	nb_ligne;
 	int	buff_read;
 	int	i;
+	int flag;
 
+	flag = 0;
 	i = 0;
-	nb_ligne = ft_test_tab_line(tab_line[fd], &i);
-	if (nb_ligne > 0)
-	{
-		line[0] = ft_strsub(tab_line[fd], i, nb_ligne);
-		tmp = ft_strsub(tab_line[fd], nb_ligne + i, ft_strlen(tab_line[fd]));
-		free(tab_line[fd]);
-		tab_line[fd] = tmp;
-		return (1);
-	}
-	else
-	{
-		while ((buff_read = read(fd, buf, BUFF_SIZE)) > 0)
+	buff_read = 1;
+	if (fd < 0)
+		return (-1);
+	if (tab_line[fd] != NULL)
+		if (tab_line[fd][0] == '\0')
 		{
-			buf[buff_read] = '\0';
-			if ((nb_ligne = ft_test_tab_line(tab_line[fd], &i)) == -1)
-				tab_line[fd] = ft_strdup(buf);
-			else if (nb_ligne == 0)
+			line[0] = NULL;
+			return (0);
+		}
+	nb_ligne = ft_test_tab_line(tab_line[fd], &i);
+	while ((nb_ligne <= 0 && buff_read != 0) || flag == 0)
+	{
+		if (nb_ligne <= 0)
+		{
+			while ((buff_read = read(fd, buf, BUFF_SIZE)) > 0 && nb_ligne <= 0)
 			{
-				tmp = ft_strjoin(tab_line[fd], buf);
-				free(tab_line[fd]);
-				tab_line[fd] = ft_strdup(tmp);
-			}
-			if ((nb_ligne = ft_test_tab_line(tab_line[fd], &i)) > 0)
-			{
-				line[0] = ft_strsub(tab_line[fd], i, nb_ligne);
-				tmp = ft_strsub(tab_line[fd], nb_ligne + i, ft_strlen(tab_line[fd]));
-				free(tab_line[fd]);
-				tab_line[fd] = tmp;
-				return (1);
+				buf[buff_read] = '\0';
+				if ((nb_ligne = ft_test_tab_line(tab_line[fd], &i)) == -1)
+					tab_line[fd] = ft_strdup(buf);
+				else if (nb_ligne == 0)
+				{
+					tmp = ft_strjoin(tab_line[fd], buf);
+					free(tab_line[fd]);
+					tab_line[fd] = ft_strdup(tmp);
+				}
+				if ((nb_ligne = ft_test_tab_line(tab_line[fd], &i)) > 0)
+					break;
 			}
 		}
+		if (buff_read == -1)
+			return (-1);
+		if (nb_ligne == 0 && buff_read == 0)
+		{
+			line[0] = ft_strdup(tab_line[fd]);
+			free(tab_line[fd]);
+			tab_line[fd] = NULL;
+			flag = 1;
+		}
+		if (nb_ligne > 0 && flag == 0)
+		{
+			line[0] = ft_strsub(tab_line[fd], i, nb_ligne);
+			tmp = ft_strsub(tab_line[fd], nb_ligne + i, ft_strlen(tab_line[fd]));
+			free(tab_line[fd]);
+			tab_line[fd] = tmp;
+			flag = 1;
+		}
+		nb_ligne = ft_test_tab_line(tab_line[fd], &i);
 	}
+	if (buff_read == 0 && nb_ligne == -1)
+	{
+		tab_line[fd] = ft_strdup("");
+		return (0);
+	}
+	else if (buff_read > 0 || nb_ligne != 0)
+			return (1);
 	return (-1);
 }
 
@@ -69,28 +94,19 @@ int	main(void)
 {
 	char	*test;
 	int	fd;
+	int i;
 
+	i = 0;
 	fd = 0;
-	if ((fd = open("get_next_line.h", O_RDONLY)) != -1)
+	if ((fd = open("one_big_fat_line.txt", O_RDONLY)) != -1)
 	{
-		if (get_next_line(fd, &test) == 1)
-		{	
+		while (i != 2)
+		{
+			printf("%d\n", get_next_line(fd, &test));
 			printf("%s\n", test);
-			if (get_next_line(fd, &test) == 1)
-			{	
-				printf("%s\n", test);
-				if (get_next_line(fd, &test) == 1)
-				{	
-					printf("%s\n", test);
-				}
-				else
-					printf("%s\n", "erreur lecture ligne 3");
-			}
-			else
-				printf("%s\n", "erreur lecture ligne 2");
+			i++;
 		}
-		else
-			printf("%s\n", "erreur lecture ligne 1");
+		printf("%d\n", i);
 	}
 	else
 		printf("%s\n", "erreur ouverture");
--- get_next_line.h
+++ get_next_line.h
@@ -1,7 +1,7 @@
 #ifndef GET_NEXT_LINE
 
 # define GET_NEXT_LINE
-# define BUFF_SIZE 90
+# define BUFF_SIZE 4
 # include <stdlib.h>
 # include <unistd.h>
 # include <sys/types.h>
