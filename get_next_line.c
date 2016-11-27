81cdc0e31ac573a09cf3a0c1ecaa2cd7fad63847
Remy CHOFFARDET <rchoffar@e1r4p12.42.fr>
2016-11-27 06:45:30 +0100

    test

--- get_next_line.c
+++ get_next_line.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rchoffar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/27 05:32:50 by rchoffar          #+#    #+#             */
+/*   Updated: 2016/11/27 06:42:31 by rchoffar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-int	ft_test_tab_line(char *str, int *i)
+int		ft_ttl(char *str, int *i)
 {
 	int	j;
 
@@ -8,19 +20,19 @@ int	ft_test_tab_line(char *str, int *i)
 	j = 0;
 	if (str != NULL)
 	{
-		while (str[*i] == '\n')
+		if (str[*i] == '\n')
 			*i = *i + 1;
 		while (str[*i + j] != '\0' && str[*i + j] != '\n')
 			j++;
-		if (str[*i + j] == '\n' && j != 0)
-			return (j);
+		if (str[*i + j] == '\n')
+			return (j + 1);
 		else if (j != 0)
 			return (0);
 	}
 	return (-1);
 }
 
-int	lecture(const int fd, char **tmp, int i)
+int		lecture(const int fd, char **tmp, int i)
 {
 	int		ret;
 	char	buff[BUFF_SIZE + 1];
@@ -30,7 +42,7 @@ int	lecture(const int fd, char **tmp, int i)
 	while ((ret = read(fd, buff, BUFF_SIZE)) > 0)
 	{
 		buff[ret] = '\0';
-		nb_line = ft_test_tab_line(tmp[0], &i);
+		nb_line = ft_ttl(tmp[0], &i);
 		if (nb_line >= 0)
 		{
 			tamp = ft_strdup(tmp[0]);
@@ -53,48 +65,54 @@ char	*set_line(char **str)
 	int		nb_line;
 	int		i;
 
-	nb_line = ft_test_tab_line(str[0], &i);
-	line = ft_strsub(str[0], i, nb_line);
-	tmp = ft_strsub(str[0], nb_line + i, ft_strlen(str[0]));
+	nb_line = ft_ttl(str[0], &i);
+	line = ft_strsub(str[0], i, nb_line - 1);
+	tmp = ft_strsub(str[0], nb_line + i - 1, ft_strlen(str[0]));
 	free(str[0]);
 	str[0] = ft_strdup(tmp);
 	free(tmp);
 	return (line);
 }
 
-int	get_next_line(const int fd, char **line)
+char	*set_end(int *etat, char **str, int nb_line)
+{
+	char	*line;
+
+	*etat = 1;
+	line = ft_strsub(str[0], nb_line, ft_strlen(str[0]));
+	free(str[0]);
+	return (line);
+}
+
+int		get_next_line(const int fd, char **line)
 {
 	static t_tab	tab[255];
-	int				retour;
-	int				nb_line;
-	int				i;
+	t_int			var;
 
-	i = 0;
-	if (fd >= 0 && tab[fd].etat == 0)
+	var.i = 0;
+	var.ret = 1;
+	if ((fd == 0 || fd > 2) && tab[fd].etat == 0)
 	{
-		while ((retour = lecture(fd, &tab[fd].str, i)) > 0);
-		if ((nb_line = ft_test_tab_line(tab[fd].str, &i)) == 0 && retour == 0)
-		{
-			tab[fd].etat = 1;
-			line[0] = ft_strsub(tab[fd].str, nb_line + i, ft_strlen(tab[fd].str));
-			free(tab[fd].str);
-		}
-		else if (retour == -1 && tab[fd].etat == 0 && nb_line == -1)
+		while (var.ret > 0)
+			var.ret = lecture(fd, &tab[fd].str, var.i);
+		if ((var.nb_line = ft_ttl(tab[fd].str, &var.i)) == 0 && var.ret == 0)
+			line[0] = set_end(&tab[fd].etat, &tab[fd].str, var.nb_line + var.i);
+		else if (var.ret == -1 && tab[fd].etat == 0 && var.nb_line == -1)
 			return (-1);
 		else
 			line[0] = set_line(&tab[fd].str);
-		if (retour == 0 && (nb_line = ft_test_tab_line(tab[fd].str, &i)) == -1)
+		if (var.ret == 0 && (var.nb_line = ft_ttl(tab[fd].str, &var.i)) == -1)
 			tab[fd].etat = 1;
 		return (1);
 	}
-	else if (fd >= 0 && tab[fd].etat == 1)
+	else if ((fd == 0 || fd > 2) && tab[fd].etat == 1)
 	{
 		line[0] = NULL;
 		return (0);
 	}
 	return (-1);
 }
-
+/*
 int	main(void)
 {
 	char	*test;
@@ -103,16 +121,16 @@ int	main(void)
 
 	i = 0;
 	fd = 0;
-	if ((fd = open("test", O_RDONLY)) != -1)
+	if ((fd = open("one_big_fat_line.txt", O_RDONLY)) != -1)
 	{
-		while (get_next_line(fd, &test) > 0)
+		while ((i = get_next_line(1, &test)) > 0)
 		{
-			printf("test : %s\n", test);
+			printf("%s\n", test);
 			i++;
 		}
-			printf("%d\n", i);
+			 printf("%d\n", i);
 	}
 	else
 		printf("%s\n", "erreur ouverture");
 	return (0);
-}
+}*/
